d4ccf017cb7fad468f3c8a2af5fca11227769f13
204G1A0526 <94513520+204G1A0526@users.noreply.github.com>
2022-11-22 12:37:14 +0530

    Create DLA.c

---
+++ OSLAB/DLA.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: T.Chandana
+   Roll No: 204G1A0526
+   Title of the Experiment: Dead Lock Avoidance Program
+   Date of Creation: 22-11-2022
+   Date of Execution: 22-11-2022
+*/
