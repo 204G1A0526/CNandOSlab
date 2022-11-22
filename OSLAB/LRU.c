fb0087bf37a878efea398d7b4381d52d7816c956
204g1a0526 <94513520+204g1a0526@users.noreply.github.com>
2022-11-22 12:28:16 +0530

    Created  LRU.c

---
+++ OSLAB/LRU.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: T.Chandana
+   Roll No: 204G1A0526
+   Title of the Experiment: First in First out program
+   Date of Creation: 22-11-2022
+   Date of Execution: 22-11-2022
+*/
