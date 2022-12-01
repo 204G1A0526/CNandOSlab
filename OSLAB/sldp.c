798dc90a6af4b617af572a4a3df933ecab73b173
204G1A0526 <94513520+204G1A0526@users.noreply.github.com>
2022-12-01 16:19:26 +0530

    Created sldp.c

---
+++ OSLAB/sldp.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: T. Chandana
+   Roll No: 204G1A0526
+   Title of the Experiment: Second Level Directory 
+   Date of Creation: 01-12-2022
+   Date of Execution: 01-12-2022
+*/
